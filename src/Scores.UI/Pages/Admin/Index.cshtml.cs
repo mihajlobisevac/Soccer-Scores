418a54a44bdb54a2459fdbc22449d4abcfe007ae
Mihajlo Biševac <kajler11@gmail.com>
2020-12-09 04:21:42 +0100

    Setup for client-side

---
+++ src/Scores.UI/Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Scores.UI.Pages.Admin
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
