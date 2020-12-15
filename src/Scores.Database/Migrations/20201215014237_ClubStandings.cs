5bcb7cb2e7bdeefa95f80d82a35bd38972c7c728
Mihajlo Bisevac <kajler11@gmail.com>
2020-12-15 02:43:18 +0100

    ClubStandings Migration

---
+++ src/Scores.Database/Migrations/20201215014237_ClubStandings.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Scores.Database.Migrations
+{
+    public partial class ClubStandings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ClubStandings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClubId = table.Column<int>(type: "int", nullable: false),
+                    StandingsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClubStandings", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Standings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TeamCount = table.Column<int>(type: "int", nullable: false),
+                    Deactivated = table.Column<bool>(type: "bit", nullable: false),
+                    TournamentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Standings", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ClubStandings");
+
+            migrationBuilder.DropTable(
+                name: "Standings");
+        }
+    }
+}
