8db029947fef5a048bbaca38c1223d0d4ca0845e
Mihajlo Bisevac <kajler11@gmail.com>
2021-01-01 23:31:55 +0100

    Added domain model for a Player in a Match

---
+++ src/Scores.Domain/Models/MatchPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scores.Domain.Models
+{
+    public class MatchPlayer
+    {
+        public int Id { get; set; }
+
+        public bool IsHome { get; set; }
+
+        public int MatchId { get; set; }
+        public int PlayerId { get; set; }
+    }
+}
