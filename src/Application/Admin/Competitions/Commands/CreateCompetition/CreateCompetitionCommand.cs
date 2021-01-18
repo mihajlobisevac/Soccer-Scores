3b3728191ec43f2fb740a9dfc46a6554aff36832
Mihajlo Bisevac <kajler11@gmail.com>
2021-01-18 05:00:25 +0100

    Added command for creating a competition

---
+++ src/Application/Admin/Competitions/Commands/CreateCompetition/CreateCompetitionCommand.cs
@@ -0,0 +1,44 @@
+﻿using Domain.Enums;
+using MediatR;
+using SoccerScores.Application.Common.Exceptions;
+using SoccerScores.Application.Common.Interfaces;
+using SoccerScores.Domain.Entities;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SoccerStreams.Application.Admin.Competitions.Commands.CreateCompetition
+{
+    public class CreateCompetitionCommand : IRequest<int>
+    {
+        public string Name { get; set; }
+        public int Type { get; set; }
+        public int CountryId { get; set; }
+    }
+
+    public class CreateCompetitionCommandHandler : IRequestHandler<CreateCompetitionCommand, int>
+    {
+        private readonly IApplicationDbContext context;
+
+        public CreateCompetitionCommandHandler(IApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<int> Handle(CreateCompetitionCommand request, CancellationToken cancellationToken)
+        {
+            var entity = new Competition
+            {
+                Name = request.Name,
+                Type = (CompetitionType)request.Type,
+                Country = await context.Countries.FindAsync(request.CountryId) ??
+                    throw new NotFoundException(nameof(Country), request.CountryId)
+            };
+
+            context.Competitions.Add(entity);
+
+            await context.SaveChangesAsync(cancellationToken);
+
+            return entity.Id;
+        }
+    }
+}
--- src/WebUI/Controllers/CompetitionsController.cs
+++ src/WebUI/Controllers/CompetitionsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SoccerScores.Application.Admin.Competitions.Queries;
+using SoccerStreams.Application.Admin.Competitions.Commands.CreateCompetition;
 using System.Threading.Tasks;
 
 namespace SoccerScores.WebUI.Controllers
@@ -12,11 +13,16 @@ namespace SoccerScores.WebUI.Controllers
             return await Mediator.Send(new GetCompetitionsQuery());
         }
 
-
         [HttpGet("{id}")]
         public async Task<ActionResult<CompetitionDto>> Get(int id)
         {
             return await Mediator.Send(new GetCompetitionQuery { Id = id });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Create(CreateCompetitionCommand command)
+        {
+            return await Mediator.Send(command);
         }
     }
 }
