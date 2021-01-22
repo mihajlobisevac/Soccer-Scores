a5ef802bbf053173a3573971631f1b0c375ad1f6
Mihajlo Bisevac <kajler11@gmail.com>
2021-01-22 01:00:07 +0100

    Added command for creating a batch of incidents for a match
    Each incident needs to have a valid match, primary and secondary players are optional

---
+++ src/Application/Admin/Matches/Commands/CreateIncidents/IncidentVm.cs
@@ -0,0 +1,15 @@
+﻿namespace SoccerScores.Application.Admin.Matches.Commands.CreateIncidents
+{
+    public class IncidentVm
+    {
+        public int HomeScore { get; set; }
+        public int AwayScore { get; set; }
+        public int Minute { get; set; }
+        public string Type { get; set; }
+        public string Class { get; set; }
+        public bool IsHome { get; set; }
+        public int MatchId { get; set; }
+        public int? PrimaryPlayerId { get; set; }
+        public int? SecondaryPlayerId { get; set; }
+    }
+}
---
+++ src/WebUI/Controllers/IncidentsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoccerScores.Application.Admin.Matches.Commands.CreateIncidents;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SoccerScores.WebUI.Controllers
+{
+    public class IncidentsController : ApiControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult<IEnumerable<int>>> Create(BatchCreateIncidentsCommand command)
+        {
+            return await Mediator.Send(command);
+        }
+    }
+}
