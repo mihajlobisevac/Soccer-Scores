d9a651dc031c874904032284cdc031650a169668
Mihajlo Bisevac <kajler11@gmail.com>
2021-01-22 01:31:37 +0100

    Added command for updating an incident

--- src/WebUI/Controllers/IncidentsController.cs
+++ src/WebUI/Controllers/IncidentsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SoccerScores.Application.Admin.Matches.Commands.CreateIncidents;
+using SoccerScores.Application.Admin.Matches.Commands.UpdateIncident;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -17,6 +18,19 @@ namespace SoccerScores.WebUI.Controllers
         public async Task<ActionResult<IEnumerable<int>>> Create(BatchCreateIncidentsCommand command)
         {
             return await Mediator.Send(command);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, UpdateIncidentCommand command)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest();
+            }
+
+            await Mediator.Send(command);
+
+            return NoContent();
         }
     }
 }
