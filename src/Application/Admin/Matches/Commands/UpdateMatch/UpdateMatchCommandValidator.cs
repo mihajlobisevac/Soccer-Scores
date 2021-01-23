031f1f0ac66f6acd121aaf62ae04cd52eef36412
Mihajlo Bisevac <kajler11@gmail.com>
2021-01-23 03:58:27 +0100

    Added validators for match commands and queries

---
+++ src/Application/Admin/Matches/Commands/UpdateMatch/UpdateMatchCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using SoccerScores.Application.Common.Interfaces;
+using System;
+using System.Linq;
+
+namespace SoccerScores.Application.Admin.Matches.Commands.UpdateMatch
+{
+    public class UpdateMatchCommandValidator : AbstractValidator<UpdateMatchCommand>
+    {
+        public UpdateMatchCommandValidator(IApplicationDbContext context)
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty()
+                .Must(id => context.Matches.Any(m => m.Id == id)).WithMessage("Match does not exist.");
+
+            RuleFor(x => x.KickOff)
+                .Must(BeAValidDate).WithMessage("Invalid date/time format.");
+        }
+
+        private bool BeAValidDate(string value)
+        {
+            return DateTime.TryParse(value, out _);
+        }
+    }
+}
