d9984440dc0a4b65ab0cb6c960afebb75eaf349e
Mihajlo Bisevac <kajler11@gmail.com>
2021-02-14 02:29:18 +0100

    competitions panel index page

---
+++ src/WebUI/wwwroot/js/admin/competitions.js
@@ -0,0 +1,27 @@
+﻿var app = new Vue({
+    el: '#app',
+    data: {
+        loading: false,
+        countries: []
+    },
+    mounted() {
+        this.getCountries();
+    },
+    methods: {
+        getCountries() {
+            this.loading = true;
+            axios.get('/api/competitions/all')
+                .then(res => {
+                    this.countries = res.data
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+                .then(() => {
+                    this.loading = false
+                })
+        }
+    },
+    computed: {
+    }
+});
