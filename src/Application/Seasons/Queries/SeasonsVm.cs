400b8ad376efe50996dfdbd717d8f69ab6ae6109
Mihajlo Bisevac <kajler11@gmail.com>
2021-02-14 04:37:19 +0100

    edit season from competition edit page

--- src/Application/Seasons/Queries/SeasonsVm.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace SoccerScores.Application.Seasons.Queries
-{
-    public class SeasonsVm
-    {
-        public IEnumerable<SeasonDto> Seasons { get; set; }
-    }
-}
