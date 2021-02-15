fa170172f85c3acc10f9aeeead9f696e9b2d42aa
Mihajlo Bisevac <kajler11@gmail.com>
2021-02-15 02:22:06 +0100

    create club page

---
+++ src/Application/Cities/Queries/GetCitiesByCountry/GetCitiesByCountryQuery.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SoccerScores.Application.Common.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SoccerScores.Application.Cities.Queries.GetCitiesByCountry
+{
+    public class GetCitiesByCountryQuery : IRequest<IEnumerable<CityDto>>
+    {
+        public int CountryId { get; set; }
+    }
+
+    public class GetCitiesByCountryQueryHandler : IRequestHandler<GetCitiesByCountryQuery, IEnumerable<CityDto>>
+    {
+        private readonly IApplicationDbContext context;
+        private readonly IMapper mapper;
+
+        public GetCitiesByCountryQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public async Task<IEnumerable<CityDto>> Handle(GetCitiesByCountryQuery request, CancellationToken cancellationToken)
+        {
+            return await context.Cities
+                .Where(x => x.Country.Id == request.CountryId)
+                .ProjectTo<CityDto>(mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
