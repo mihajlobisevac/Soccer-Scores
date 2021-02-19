4cc0ef5318307ff65241d0561980e907b3152c15
Mihajlo Bisevac <kajler11@gmail.com>
2021-02-19 01:55:39 +0100

    matches index page

---
+++ src/Application/Competitions/Queries/GetCompetitionsByCountry/CompetitionDto.cs
@@ -0,0 +1,11 @@
+﻿using SoccerScores.Application.Common.Mappings;
+using SoccerScores.Domain.Entities;
+
+namespace SoccerScores.Application.Competitions.Queries.GetCompetitionsByCountry
+{
+    public class CompetitionDto : IMapFrom<Competition>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
