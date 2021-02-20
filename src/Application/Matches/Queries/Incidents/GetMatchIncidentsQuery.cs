f8bda97f2a1d4d1f72b3fb13c7017c999e63df38
Mihajlo Bisevac <kajler11@gmail.com>
2021-02-20 01:47:40 +0100

    query for getting all incidents of a match

---
+++ src/Application/Matches/Queries/Incidents/GetMatchIncidentsQuery.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SoccerScores.Application.Common.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SoccerScores.Application.Matches.Queries.Incidents
+{
+    public class GetMatchIncidentsQuery : IRequest<IEnumerable<IncidentDto>>
+    {
+        public int MatchId { get; set; }
+    }
+
+    public class GetMatchIncidentsQueryHandler : IRequestHandler<GetMatchIncidentsQuery, IEnumerable<IncidentDto>>
+    {
+        private readonly IApplicationDbContext context;
+        private readonly IMapper mapper;
+
+        public GetMatchIncidentsQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public async Task<IEnumerable<IncidentDto>> Handle(GetMatchIncidentsQuery request, CancellationToken cancellationToken)
+        {
+            return await context.Incidents
+                .Where(x => x.Match.Id == request.MatchId)
+                .ProjectTo<IncidentDto>(mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
