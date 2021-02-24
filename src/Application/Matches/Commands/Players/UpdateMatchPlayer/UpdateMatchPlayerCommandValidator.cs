8cf88644dd9a5b47a9c11a2b2c8928073d7d2e5d
Mihajlo Bisevac <kajler11@gmail.com>
2021-02-24 01:01:23 +0100

    update player in lineup command and controller

---
+++ src/Application/Matches/Commands/Players/UpdateMatchPlayer/UpdateMatchPlayerCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using SoccerScores.Application.Common.Interfaces;
+using System.Linq;
+
+namespace SoccerScores.Application.Matches.Commands.Players.UpdateMatchPlayer
+{
+    public class UpdateMatchPlayerCommandValidator : AbstractValidator<UpdateMatchPlayerCommand>
+    {
+        public UpdateMatchPlayerCommandValidator(IApplicationDbContext context)
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty()
+                .Must(id => context.MatchPlayers.Any(i => i.Id == id)).WithMessage("Chosen match player does not exist.");
+
+            RuleFor(x => x.IsHome)
+                .NotNull();
+
+            RuleFor(x => x.IsSubstitute)
+                .NotNull();
+
+            RuleFor(x => x.PlayerId)
+                .NotEmpty()
+                .Must(id => context.Players.Any(i => i.Id == id)).WithMessage("Chosen player does not exist.");
+        }
+    }
+}
---
+++ src/WebUI/Controllers/LineupsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoccerScores.Application.Matches.Commands.Players.UpdateMatchPlayer;
+using System.Threading.Tasks;
+
+namespace SoccerScores.WebUI.Controllers
+{
+    public class LineupsController : ApiControllerBase
+    {
+        [HttpPut("player/{id}")]
+        public async Task<ActionResult> Update(int id, UpdateMatchPlayerCommand command)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest();
+            }
+
+            await Mediator.Send(command);
+
+            return NoContent();
+        }
+    }
+}
