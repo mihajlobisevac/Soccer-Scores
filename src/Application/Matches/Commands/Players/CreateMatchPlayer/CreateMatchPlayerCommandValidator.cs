e4945b5234c1cde2371aba0d3aa441cb29139fcf
Mihajlo Bisevac <kajler11@gmail.com>
2021-02-24 01:56:47 +0100

    create new player in match lineup

---
+++ src/Application/Matches/Commands/Players/CreateMatchPlayer/CreateMatchPlayerCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using SoccerScores.Application.Common.Interfaces;
+using System.Linq;
+
+namespace SoccerScores.Application.Matches.Commands.Players.CreateMatchPlayer
+{
+    public class CreateMatchPlayerCommandValidator : AbstractValidator<CreateMatchPlayerCommand>
+    {
+        public CreateMatchPlayerCommandValidator(IApplicationDbContext context)
+        {
+            RuleFor(x => x.IsHome)
+                .NotNull();
+
+            RuleFor(x => x.IsSubstitute)
+                .NotNull();
+
+            RuleFor(x => x.PlayerId)
+                .NotEmpty()
+                .Must(id => context.Players.Any(i => i.Id == id)).WithMessage("Chosen player does not exist.");
+
+            RuleFor(x => x.MatchId)
+                .NotEmpty()
+                .Must(id => context.Matches.Any(i => i.Id == id)).WithMessage("Chosen match does not exist.");
+        }
+    }
+}
--- src/WebUI/Controllers/LineupsController.cs
+++ src/WebUI/Controllers/LineupsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SoccerScores.Application.Matches.Commands.Players.CreateMatchPlayer;
 using SoccerScores.Application.Matches.Commands.Players.UpdateMatchPlayer;
 using System.Threading.Tasks;
 
@@ -6,8 +7,15 @@ namespace SoccerScores.WebUI.Controllers
 {
     public class LineupsController : ApiControllerBase
     {
+
+        [HttpPost("player")]
+        public async Task<ActionResult<int>> CreatePlayer(CreateMatchPlayerCommand command)
+        {
+            return await Mediator.Send(command);
+        }
+
         [HttpPut("player/{id}")]
-        public async Task<ActionResult> Update(int id, UpdateMatchPlayerCommand command)
+        public async Task<ActionResult> UpdatePlayer(int id, UpdateMatchPlayerCommand command)
         {
             if (id != command.Id)
             {
